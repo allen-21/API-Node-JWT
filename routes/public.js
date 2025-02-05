8b542fa97b0b45558c449473844b840d0e466984
allen-21 <valteranibal1@gmail.com>
2025-02-05 20:57:42 +0200

    cadastro usuarios

--- routes/public.js
+++ routes/public.js
@@ -1,12 +1,30 @@
-import express from 'express'
+import express from "express";
+import bcrypt from "bcrypt";
+import { PrismaClient } from "@prisma/client/extension";
 
-const router = express.Router()
+const router = express.Router();
+const prisma = new PrismaClient();
 
-// Cadastro 
-router.post('/cadastro', (req, res) => {
-    const user = req.body
+// Cadastro
+router.post("/cadastro", async (req, res) => {
+  try {
+    const user = req.body;
 
-    res.status(201).json(user)
-})
+    const salt = await bcrypt.genSalt(10);
+    const hashPassword = await bcrypt.hash(user.password, salt);
 
-export default router
+    const userDB = await prisma.user.create({
+      data: {
+        email: user.email,
+        name: user.name,
+        password: hashPassword,
+      },
+    });
+
+    res.status(201).json(userDB);
+  } catch (error) {
+    res.status(500).json({ massege: "erro no servidor tente novamente" });
+  }
+});
+
+export default router;
