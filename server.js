c0371b4298e1a25280e8d62876caa49c3b7dd19d
allen-21 <valteranibal1@gmail.com>
2025-02-05 20:32:06 +0200

    update

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
-import express from 'express'
+import express from "express";
+import publicRoutes from "./routes/public.js";
+
+const app = express();
+app.use(express.json());
+
+app.use("/", publicRoutes);
+
+app.listen(8080, () => console.log("Servidor rodando"));
