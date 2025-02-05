afb31bd35ae542373bbc182635219e4dd9fce79e
allen-21 <valteranibal1@gmail.com>
2025-02-05 22:21:37 +0200

    adcionar o login padrao

--- routes/public.js
+++ routes/public.js
@@ -1,6 +1,7 @@
 import express from "express";
 import bcrypt from "bcrypt";
-import { PrismaClient } from "@prisma/client/extension";
+import { PrismaClient } from "@prisma/client";
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
 const prisma = new PrismaClient();
@@ -24,6 +25,36 @@ router.post("/cadastro", async (req, res) => {
     res.status(201).json(userDB);
   } catch (error) {
     res.status(500).json({ massege: "erro no servidor tente novamente" });
+  }
+});
+
+//login
+
+router.post("/login", async (req, res) => {
+  try {
+    const userInfo = req.body;
+    //Busca o usuario no banco de dados
+    const user = await prisma.user.findUnique({
+      where: { email: userInfo.email },
+    });
+
+    //Verifica se o usuario existe
+    if (!user) {
+      return res.status(404).json({ message: "Usuario nao encontrado" });
+    }
+
+    const isMatch = await bcrypt.compare(userInfo.password, user.password);
+
+    //verifica a senha ou compara as senhas
+    if (!isMatch) {
+      return res.status(400).json({ message: "senha invalida" });
+    }
+
+    // gerar o token JWT
+
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ message: "erro no servidor tente novamente" });
   }
 });
 
